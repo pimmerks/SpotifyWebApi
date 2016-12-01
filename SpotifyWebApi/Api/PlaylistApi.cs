3526dde3fdc89b9f963935a5cdeccdb0284624b4
ppiimm123 <pimmerks@msn.com>
2016-12-01 12:24:32 +0100

    Added some api classes

---
+++ SpotifyWebApi/Api/PlaylistApi.cs
@@ -0,0 +1,29 @@
+﻿using SpotifyWebApi.Model;
+using SpotifyWebApi.Model.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyWebApi.Api
+{
+    public class PlaylistApi
+    {
+        public static FullPlaylist GetPlaylist(string playlistId, string userId, Token token)
+        {
+
+            //FullPlaylist playlist = null;
+            //var json = Api.GetJsonFromUrl("https://api.spotify.com/v1/users/" + userId + "/playlists/" + playlistId, token);
+            //playlist = JsonToPlaylist(json);
+            //return playlist;
+            return null;
+        }
+
+        public static FullPlaylist GetPlaylistFromUri(string uri, Token token)
+        {
+            return null;
+
+        }
+    }
+}
---
+++ SpotifyWebApi/Api/TrackApi.cs
@@ -0,0 +1,26 @@
+﻿using SpotifyWebApi.Model;
+using SpotifyWebApi.Model.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyWebApi.Api
+{
+    public class TrackApi
+    {
+        public static FullTrack GetTrack(string id, Token accestoken)
+        {
+            return ApiHelper.GetObjectFromUrl<FullTrack>("https://api.spotify.com/v1/tracks/" + id, accestoken);
+        }
+
+        public static FullTrack GetTrackFromUri(string uri, Token accestoken)
+        {
+            var id = uri.Split(':')[2];
+            return ApiHelper.GetObjectFromUrl<FullTrack>("https://api.spotify.com/v1/tracks/" + id, accestoken);
+        }
+
+
+    }
+}
