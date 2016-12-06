db096c6c84679e64a5cc84cb91d2b038ff437e0c
ppiimm123 <pimmerks@msn.com>
2016-12-06 17:12:43 +0100

    Added SpotifyUri and UriType Updated AlbumApi to support this

--- SpotifyWebApi/Api/AlbumApi.cs
+++ SpotifyWebApi/Api/AlbumApi.cs
@@ -1,5 +1,6 @@
 ﻿using SpotifyWebApi.Model;
 using SpotifyWebApi.Model.Auth;
+using SpotifyWebApi.Model.Uri;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +13,13 @@ namespace SpotifyWebApi.Api
     {
         public static FullAlbum GetAlbum(string uri, Token token)
         {
-            var id = uri.Split(':')[2];
-            return ApiHelper.GetObjectFromUrl<FullAlbum>("https://api.spotify.com/v1/albums/" + id, token);
+            var spotifyUri = new SpotifyUri(uri);
+            return GetAlbum(spotifyUri, token);
+        }
+
+        public static FullAlbum GetAlbum(SpotifyUri uri, Token token)
+        {
+            return ApiHelper.GetObjectFromUrl<FullAlbum>("https://api.spotify.com/v1/albums/" + uri.Id, token);
         }
     }
 }
---
+++ SpotifyWebApi/Model/Uri/SpotifyUri.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyWebApi.Model.Uri
+{
+    public class SpotifyUri
+    {
+        public string Domain { get; private set; }
+
+        public UriType Type { get; private set; }
+
+        public string User { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string FullUri { get; private set; }
+
+        public SpotifyUri(string uri)
+        {
+            FullUri = uri;
+
+            var split = uri.Split(':');
+            Domain = split[0];
+
+            if (Domain.Equals("spotify"))
+            {
+                if (split.Length == 5)
+                    Type = UriType.playlist;
+                else
+                    Type = (UriType)Enum.Parse(typeof(UriType), split[1]);
+                
+                switch (Type)
+                {
+                    case UriType.user:
+                        Id = split[2];
+                        break;
+                    case UriType.track:
+                        Id = split[2];
+                        break;
+                    case UriType.artist:
+                        Id = split[2];
+                        break;
+                    case UriType.album:
+                        Id = split[2];
+                        break;
+                    case UriType.playlist:
+                        User = split[2];
+                        Id = split[4];
+                        break;
+                    default:
+                        throw new Exception("Uri was not correct!");
+                }
+            }
+            else
+            {
+                throw new Exception("Uri was not a spotify uri!");
+            }
+        }
+    }
+}
