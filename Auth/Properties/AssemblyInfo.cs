28556be96740138c27bbc41ae88571b86ad48b01
Pim Merks <pimmerks@msn.com>
2018-02-13 16:16:12 +0100

    Updated ci configuration and updated deployment task

--- Auth/Properties/AssemblyInfo.cs
+++ Auth/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Auth")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Spotify Web Api Authentication")]
+[assembly: AssemblyDescription("Spotify Web Api")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Auth")]
+[assembly: AssemblyProduct("Spotify Web Api Authentication")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0")]
-[assembly: AssemblyFileVersion("0.0")]
-[assembly: AssemblyInformationalVersion("{semvertag}-{chash:8}")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
